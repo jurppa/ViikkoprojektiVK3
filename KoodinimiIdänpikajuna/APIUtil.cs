8a9aeedb9f6ebd7a9b2d40955abedcc801f94028
jurppa <78047234+jurppa@users.noreply.github.com>
2021-07-05 11:11:55 +0300

    Aloitettu tiedonkäsittelyä

--- KoodinimiIdänpikajuna/APIUtil.cs
+++ KoodinimiIdänpikajuna/APIUtil.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using KoodinimiIdänpikajuna.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KoodinimiIdänpikajuna
 {
-    class Class1
+    class APIUtil
     {
+        private const string APIURL = "https://rata.digitraffic.fi/api/v1";
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task ProcessRequests()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("accept-encoding", "gzip");
+            var response = client.GetAsync($"{APIURL}/metadata/stations").Result;
+            string json = response.Content.ReadAsStringAsync().Result;
+            List<Station> res = JsonConvert.DeserializeObject<List<Liikennepaikka>>(json);
+        }
+            
+
+
     }
 }
