48d60502b9a24effcb743a3f06bff685711d5242
GitMatias <86785955+GitMatias@users.noreply.github.com>
2021-07-05 11:09:42 +0300

    Station luokka/ Matias

--- KoodinimiIdänpikajuna/Model/Station.cs
+++ KoodinimiIdänpikajuna/Model/Station.cs
@@ -8,5 +8,13 @@ namespace KoodinimiIdänpikajuna.Model
 {
     class Station
     {
+        public bool passengerTraffic;//    passengerTraffic: boolean Onko liikennepaikalla kaupallista matkustajaliikennettä
+        public string countryCode;//countryCode: string Liikennepaikan maatunnus
+        public string stationName;//stationName: string Liikennepaikan nimi
+        public string stationShortCode;//stationShortCode: string Liikennepaikan lyhenne
+        public int stationUICCode;//stationUICCode: 1-9999   Liikennepaikan maakohtainen UIC-koodi
+        public decimal latitude;//latitude: decimal Liikennepaikan latitude "WGS 84"-muodossa
+        public decimal longitude;//longitude: decimal Liikennepaikan longitudi "WGS 84"-muodossa
+        public string type;//type: string Liikennepaikan tyyppi.STATION = asema, STOPPING_POINT = seisake, TURNOUT_IN_THE_OPEN_LINE = linjavaihde
     }
 }
