8c5fb7e4ef9519cdcc374debe7c171fa27fa9c10
jurppa <78047234+jurppa@users.noreply.github.com>
2021-07-06 16:26:51 +0300

    Testi vaunun palveluille

--- AppUnitTests/UnitTest1.cs
+++ AppUnitTests/UnitTest1.cs
@@ -1,7 +1,6 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using KoodinimiIdänpikajuna;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.IO;
 using System.Collections.Generic;
 namespace AppUnitTests
 {
@@ -12,21 +11,21 @@ namespace AppUnitTests
         public void AreNamesEqualShouldReturnTrue()
         {
 
-         string[] expected = { "TPE", "HKI" };
-         string[] actual = APIUtil.GetStationFullNames("Tampere","Helsinki");
-        
+            string[] expected = { "TPE", "HKI" };
+            string[] actual = APIUtil.GetStationFullNames("Tampere", "Helsinki");
 
-         Assert.AreEqual(expected[0], actual[0]);
-         Assert.AreEqual(expected[1], actual[1]);
-    
+
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+
 
 
         }
         [TestMethod]
         public void TestMethod2()
         {
-        APIUtil.TrainFromTo("Helsinki" , "Tampere");
-        
+            APIUtil.TrainFromTo("Helsinki", "Tampere");
+           
 
         }
 
@@ -36,17 +35,12 @@ namespace AppUnitTests
             Dictionary<string, bool> servicesInWagons = new Dictionary<string, bool>();
             servicesInWagons.Add("Catering", true);
             servicesInWagons.Add("Lasten leikkialue", true);
-       
+
             servicesInWagons.Add("Tupakointi", false);
             servicesInWagons.Add("Lemmikkipaikka", true);
-
-
             Dictionary<string, bool> actualServicesInWagons = APIUtil.GetWagonInfo(DateTime.Now.Date, 150);
-
             Assert.AreEqual(servicesInWagons["Catering"], actualServicesInWagons["Catering"]);
             Assert.AreEqual(servicesInWagons["Lasten leikkialue"], actualServicesInWagons["Lasten leikkialue"]);
-         
-            
             Assert.AreEqual(servicesInWagons["Lemmikkipaikka"], actualServicesInWagons["Lemmikkipaikka"]);
 
 
