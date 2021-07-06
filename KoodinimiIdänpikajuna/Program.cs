5eb54563996c593d9615ca6652242121286336cf
Tblast <nguyen-thien@outlook.com>
2021-07-06 09:15:14 +0300

    Alkuvalikko siirretty UI luokkaan

--- KoodinimiIdänpikajuna/Program.cs
+++ KoodinimiIdänpikajuna/Program.cs
@@ -6,34 +6,9 @@ namespace KoodinimiIdänpikajuna
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("1. Juna aikataulut");
-            Console.WriteLine("2. Seuraavaksi lähtevä juna");
-            Console.WriteLine("3. Tietoa junan palveluista");
-            Console.WriteLine("4. Junan välipysäkit: ");
-            Console.Write("Valitse toiminta syöttämällä oikea numero: ");
-
-            int input = Int32.Parse(Console.ReadLine());
             UI ui = new UI();
-
-            switch (input)
-            {
-                case 1:
-                    ui.FromTo();
-                    break;
-                case 2:
-                    ui.NextTrain();
-                    break;
-                case 3:
-                    ui.TrainInfo();
-                    break;
-                case 4:
-                    ui.IntermediateStation();
-                    break;
-                default:
-                    Console.WriteLine("Error 404");
-                    break;
-            }
+            ui.startMenu();
+           
             ui.TrainInfo();
            
 
