7bc47db00795bfff87bba44bd29ee4a920f9a5ff
Tblast <nguyen-thien@outlook.com>
2021-07-08 13:28:10 +0300

    Testi kommentit poistettu programmista

--- KoodinimiIdänpikajuna/Program.cs
+++ KoodinimiIdänpikajuna/Program.cs
@@ -6,22 +6,8 @@ namespace KoodinimiIdänpikajuna
     {
         static void Main(string[] args)
         {
-          
             UI ui = new UI();
             ui.StartMenu();
-            
-
-
-            //var testi = APIUtil.GoingThrough("PRI");
-            //ui.StartMenu();
-            //APIUtil.TrackLiveTrainLocation(150);
-
-            //var testi = APIUtil.TrackLiveTrainLocation(150);
-            //Console.WriteLine(testi.nextStation);
-            //Console.WriteLine(testi.station);
-            //Console.WriteLine(testi.previousStation);
         }
-
-
     }
 }
